fce5ca4f849368a772471aa49621427417342272
69Nesta <petitromeo74@gmail.com>
2026-01-08 17:30:26 +0100

    Feat: ft_archive_creation

---
+++ ex1/ft_archive_creation.py
@@ -0,0 +1,36 @@
+#! python3
+
+def ft_archive_creation():
+    print('=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n')
+
+    filename = 'new_discovery.txt'
+    entrys = [
+        'New quantum algorithm discovered',
+        'Efficiency increased by 347%',
+        'Archived by Data Archivist trainee'
+    ]
+
+    try:
+        print(f'Initializing new storage unit: {filename}')
+        with open(filename, 'w') as file:
+            print('Storage unit created successfully...')
+            print('\nInscribing preservation data...')
+            for idx, entry in enumerate(entrys):
+                content = f'[ENTRY {idx}] {entry}'
+                file.write(content)
+                if (idx != len(entrys) - 1):
+                    file.write('\n')
+                print(content)
+            print('')
+        print('Data inscription complete. Storage unit sealed.')
+        print(f'Archive \'{filename}\' ready for long-term preservation.')
+
+    except PermissionError:
+        print('Error: Failaid to inscribing preservation data '
+              '(no permission)...')
+    except OSError as e:
+        print(f'Error: {e}')
+
+
+if __name__ == '__main__':
+    ft_archive_creation()
